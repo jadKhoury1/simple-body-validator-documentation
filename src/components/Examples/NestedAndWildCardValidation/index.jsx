1d5b912dbeeaf225f28e4889bbdaf41bdce1ca0b
Jad Khoury <jad.khoury14@gmail.com>
2022-12-16 22:58:45 +0100

    Fixed typo

--- src/components/Examples/NestedAndWildCardValidation/index.jsx
+++ src/components/Examples/NestedAndWildCardValidation/index.jsx
@@ -12,7 +12,7 @@ export default () => {
         profile: {
             firstName: '',
             lastName: '',
-            gebder: '',
+            gender: '',
             socialPlatforms: [],
             addresses: [
                 {
